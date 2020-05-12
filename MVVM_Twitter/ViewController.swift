6bbed7664e99b6a52a7de4210c4dfefc49babb0d
Ankit Saxena <iamankit1696@gmail.com>
2020-05-12 15:56:03 +0530

    Initial Commit

---
+++ MVVM_Twitter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM_Twitter
+//
+//  Created by Ankit Saxena on 12/05/20.
+//  Copyright © 2020 Ankit Saxena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
